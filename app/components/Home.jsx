1e2a14251d0f2f8d2468c3b4a588ab5388d92658
Adam Bloomston <mail@adambloomston>
2016-04-05 15:33:37 -0400

    center in Home

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 import GetWeatherContainer from '../containers/GetWeatherContainer.jsx';
 
@@ -7,8 +7,16 @@ import styles from '../styles/Home.css';
 
 var Home = props => (
 	<Grid fluid className={styles.root}>
-		<h1 className={styles.header}>Enter a City and State</h1>
-		<GetWeatherContainer/>
+    <Row>
+      <Col xs={12}>
+		    <h1 className={styles.header}>Enter a City and State</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={6} xsOffset={3} sm={6} smOffset={3} md={4} mdOffset={4} lg={2} lgOffset={5}>
+		    <GetWeatherContainer/>
+      </Col>
+    </Row>
 	</Grid>
 );
 
