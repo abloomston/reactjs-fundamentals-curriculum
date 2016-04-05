9b0ed9fec250c197eaf175e732491d1da1a8d2c1
Adam Bloomston <mail@adambloomston>
2016-04-05 11:35:31 -0400

    styling for navbar & css tweak
    added TODO to find a better way to handle styling of navbar

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Grid, Nav, Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import bUtils from 'react-bootstrap/lib/utils/bootstrapUtils'
 import {IndexLinkContainer} from 'react-router-bootstrap';
 
 import GetWeatherContainer from '../containers/GetWeatherContainer.jsx';
 
 import styles from '../styles/Main.css';
 
+bUtils.addStyle(Navbar, ['E5991CBDFC0C']); /* Override navbar-default with navbar-E5991CBDFC0C, a bogus class */
+/* TODO find a better way to handle locally scoped styling override for navbar */
+
 var Main = props => (
   <div className={styles.root}>
-    <Navbar fluid>
+    <Navbar staticTop fluid bsStyle='E5991CBDFC0C' className={styles.nav}>
       <Navbar.Header>
 			  <IndexLinkContainer to="/"><Navbar.Brand>My Weather App</Navbar.Brand></IndexLinkContainer>
       </Navbar.Header>
