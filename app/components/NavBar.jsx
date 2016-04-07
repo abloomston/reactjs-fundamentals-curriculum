0f8909928e4be08bbd604403291bedd9b9f755d9
Adam Bloomston <mail@adambloomston>
2016-04-07 17:12:21 -0400

    removed TODO
    didn't find a better approach, cf http://stackoverflow.com/questions/26434512/getting-rid-of-padding-at-the-bottom-of-bootstrap-navbar

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -9,11 +9,10 @@ import styles from '../styles/NavBar.css';
 
 bUtils.addStyle(Navbar, ['custom']); /* Use class navbar-custom, whose style is globally defined in NavBar.css */
 
-/* TODO find a better way to override margin-bottom for navbar than hack below */
 /* TODO responsive sizing of navbar.form leads to input field in GetWeatherComponent filling up screen. This is an issue with this simple react-bootstrap example as well: http://react-bootstrap.github.io/components.html#navbars-form */
 
 var NavBar = props => (
-  <Navbar staticTop fluid bsStyle='custom' style={{marginBottom: '0px'}}>
+  <Navbar staticTop fluid bsStyle='custom' style={{marginBottom: '0px'} /* override .navbar margin-bottom */}>
     <Navbar.Header>
 			<IndexLinkContainer to="/"><Navbar.Brand>My Weather App</Navbar.Brand></IndexLinkContainer>
     </Navbar.Header>
