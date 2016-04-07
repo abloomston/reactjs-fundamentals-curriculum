b1f3da0affa0f47f163a303eec028d2eb581f627
Adam Bloomston <mail@adambloomston>
2016-04-07 17:07:42 -0400

    using globally defined css for navbar-custom
    I would use the previous approach if I expected multiple navbar custom
    styles in the web-app. Given that I do not expect that here, I am
    keeping it as globally defined for simplicity of output representation (DOM/CSS).

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -7,14 +7,13 @@ import GetWeatherContainer from '../containers/GetWeatherContainer.jsx';
 
 import styles from '../styles/NavBar.css';
 
-bUtils.addStyle(Navbar, ['E5991CBDFC0C']); /* Override navbar-default with navbar-E5991CBDFC0C, a bogus class */
+bUtils.addStyle(Navbar, ['custom']); /* Use class navbar-custom, whose style is globally defined in NavBar.css */
 
-/* TODO find a better way to handle locally scoped styling override for navbar. */
 /* TODO find a better way to override margin-bottom for navbar than hack below */
 /* TODO responsive sizing of navbar.form leads to input field in GetWeatherComponent filling up screen. This is an issue with this simple react-bootstrap example as well: http://react-bootstrap.github.io/components.html#navbars-form */
 
 var NavBar = props => (
-  <Navbar staticTop fluid bsStyle='E5991CBDFC0C' className={styles.root} style={{marginBottom: '0px'}}>
+  <Navbar staticTop fluid bsStyle='custom' style={{marginBottom: '0px'}}>
     <Navbar.Header>
 			<IndexLinkContainer to="/"><Navbar.Brand>My Weather App</Navbar.Brand></IndexLinkContainer>
     </Navbar.Header>
